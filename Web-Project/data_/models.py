60d697e4f4527910d1d14cb2017c5f75d7b77f99
Snezhi <snezhana_petrova@gmail.com>
2024-04-25 14:44:01 +0300

    Group sesion/ mindF**K a.k.a BrainStorming

--- Web-Project/data_/models.py
+++ Web-Project/data_/models.py
@@ -1,65 +1,68 @@
-from pydantic import BaseModel,field_validator
+from pydantic import BaseModel, field_validator, constr, conint, EmailStr
 from datetime import date, datetime  # date because it shows the date only / in the database it has time also/
+
+
+class Role:
+
+    ADMIN = 'admin'
+    USER = 'user'
 
 class User(BaseModel):
 
-    id: int = None or None
+    id: int | None = None
     username: str    # да има ли рестрикции като на базата данни, за да не гърми там, а тук
-    password: str   #validator?
+    password: str | None = None
     first_name: str
     last_name: str
-    email: str    # email validator online through web client????
-    date_of_birth: str
-    admin_id: int
-    token_id: int
+    email: EmailStr    # email validator online through web client????
+    date_of_birth: date
+    admin_id: int | None = None
+    token_id: int | None = None
 
-    @field_validator('username')
-    def username_rest(cls, value):
-        if len(value) > 45:
-            raise ValueError('Username is too long!')
-        return value
+    @classmethod
+    def from_query_result(cls, id: int, username: str, password: str, first_name: str, last_name: str, email: str,
+                          date_of_birth: date):
+        return cls(id=id, username=username, password=password, first_name=first_name, last_name=last_name, email=email,
+                   date_of_birth=date_of_birth)
 
-    @field_validator('date_of_birth')
-    def date_restr(cls, date_value):
-
-        date_obj = datetime.strptime(date_value, '%Y-%m-%d')
-        min_date = datetime.strptime('1907-01-01', '%Y-%m-%d')
-
-        year, mount, cur_date = date_value.split('/')
-        if not len(year) == 4 and len(mount) == 2 and len(cur_date) == 2: #при положение, че месеца и деня започва с 0
-            raise ValueError('Invalid date format')
-
-        if date_obj > date.today():
-            raise ValueError('Possibly you are not born in the future')
-
-        if date_obj < min_date:
-            raise ValueError('You are not a vempire!')
-
-        return date_obj
+    def is_admin(self):
+        return self.role == Role.ADMIN
 
 
+class LoginData(BaseModel):
+
+    username: str
+    password: str
 
 
 class Admin(BaseModel):
 
-    id: int = None or None
+    id: int | None = None
     users_id: int
     category_id: int
 
 
-class Message(BaseModel): #date na message?
-    id: int = None or None
+class Message(BaseModel):
+
+    id: int | None = None
     content: str
     sender_id: int
     receiver_id: int
+    created_at: datetime = datetime.now()
+
+
+class MessageCreate(BaseModel):
+
+    content: str
+    received_id: int
 
 
 class Category(BaseModel):
 
-    id: int = None or None
+    id: int | None = None
     title: str
     description: str
-    reply_cnt: int               #Трябра ли да го има в таzи таблица или, когато го визуализираме в приложението можем да отидем до базата през таблицата на Reply
+    reply_cnt: int  # Трябра ли да го има в таzи таблица или, когато го визуализираме в приложението можем да отидем до базата през таблицата на Reply
     last_topic: str
     topic_cnt: int
     user_id: int
@@ -69,50 +72,55 @@ class Topic(BaseModel):
 
     id: int = None or None
     title: str
-    cur_date: datetime.now()    #да се сетва по дефолт на днес?
+    cur_date: datetime = datetime.now()
     reply_cnt: int
     view_cnt: int
-    last_reply: str
-    users_id: int
+    last_reply: str | None = None
+    user_id: int
     category_id: int
 
-    #
-    # @field_validator('cur_time')
-    # def set_time(cls, value):
-    #     return datetime.now()
+    @classmethod
+    def from_query_result(cls, id, title, cur_date, reply_cnt,
+                          view_cnt, last_reply, user_id, category_id):
+        return cls(
+            id=id,
+            title=title,
+            cur_date=cur_date,
+            reply_cnt= 0 if reply_cnt==None else reply_cnt,
+            view_cnt=0 if view_cnt==None else view_cnt,
+            last_reply=last_reply,
+            user_id=user_id,
+            category_id=category_id)
 
 
 class Reply(BaseModel):
 
     id: int = None or None
-    cur_date: datetime.now()
+    cur_date: datetime = datetime.now()
     content: str
-    likes_cnt: int
-    dislikes_cnt: int
+    likes_cnt: conint(ge=0)
+    dislikes_cnt: conint(ge=0)  ## Подсигурява, че не можем да имаме негативен брой ляйк/дисляйк, вместо проверка.
     topic_id: int
-    user_id: int
-    vote_id: int
 
-    # @field_validator('cur_time')
-    # def set_time(cls, value):
-    #     return datetime.now()
-
-    @field_validator('dislikes_cnt')
-    def dislike_restr(cls, value):
-        if value < 0:
-            return 0
-        return value
-
+    @classmethod
+    def from_query_result(cls, id, cur_date, content, likes_cnt, dislikes_cnt, topic_id):
+        return cls(
+            id=id,
+            cur_date=cur_date,
+            content=content,
+            likes_cnt=likes_cnt,
+            dislikes_cnt=dislikes_cnt,
+            topic_id=topic_id)
 
 
 class Vote(BaseModel):
 
-    id: int = None or None
-    upvote: int = 1
-    downvote: int = 1
+    reply_id: int = None or None
+    sender_id: int = None or None
+    vote: int
 
-    @field_validator('dislikes_cnt')
-    def downvote_restr(cls, value):
-        if value < 0:
-            return 0
-        return value
+
+class Conversation(BaseModel):
+
+    id: int | None = None
+    content: list[Message] or None
---
+++ Web-Project/services/users_service.py
@@ -0,0 +1,151 @@
+from string import punctuation, whitespace, digits, ascii_lowercase, ascii_uppercase
+from typing import Optional, List
+from data_.models import *
+from data_.database import read_query, insert_query
+from mariadb import IntegrityError
+from fastapi import HTTPException, Header, Depends
+import jwt
+from typing import Optional
+
+_SEPARATOR = ';'
+ALGORITHM = "HS256"
+
+def create(username: str, password: str, first_name: str, last_name: str, email: str,
+           date_of_birth: date) -> User | None:
+    existing_user = read_query('SELECT id FROM users WHERE username = ?', (username,))
+    if existing_user:
+        raise HTTPException(status_code=400, detail=f'Username {username} is taken.')
+
+    generated_id = insert_query(
+        'INSERT INTO users(username, password, first_name, last_name, email, date_of_birth) VALUES (?,?,?,?,?,?)',
+        (username, password, first_name, last_name, email, date_of_birth))
+
+    return User(id=generated_id, username=username, password='', first_name=first_name, last_name=last_name,
+                email=email, date_of_birth=date_of_birth)
+
+
+def all():  # само за админ
+    data = read_query('''SELECT id, username, password, first_name, last_name, email, date_of_birth FROM users''')
+    return (User.from_query_result(*row) for row in data)
+
+
+def try_login(username: str, password: str) -> User | None:
+    user = find_by_username(username)
+
+    # password = _hash_password(password)
+    return user if user and user.password == password else None
+
+
+def create_access_token(user: User) -> dict:
+    # note: this token is not particulary secure, use JWT for real-world uses
+    username = f'{user.id}{_SEPARATOR}{user.username}'
+
+    passw = [str(ord(p) + 10) for p in user.password]
+    user.password = ''.join(passw)
+
+    return {'username': username,
+            'password': user.password}
+
+
+
+def from_token(token: str) -> User | None:
+    _, username = token.split(_SEPARATOR)
+
+    return find_by_username(username)
+
+
+def find_by_username(username: str) -> User | None:
+    data = read_query(
+        '''SELECT id, username, password, first_name,
+        last_name, email, date_of_birth
+        FROM users WHERE username = ?''',
+        (username,))
+
+    return next((User.from_query_result(*row) for row in data), None)
+
+
+
+def is_authenticated(token: str) -> bool:
+    return any(read_query(
+        'SELECT 1 '
+        'FROM users '
+        'where id = ? and username = ?',
+
+        token.split(_SEPARATOR)))
+
+
+def get_token_header(token: str = Header()):
+    if not is_authenticated(token):
+        raise HTTPException(status_code=401, detail="Invalid or missing token")
+
+    return token  #!!
+
+
+
+def is_valid_password(password):
+    new_password = password.strip()
+
+    MIN_SIZE = 6
+    MAX_SIZE = 50
+    password_size = len(new_password)
+
+    if password_size < MIN_SIZE or password_size > MAX_SIZE:
+        return False
+
+    valid_chars = {'-', '_', '.', '!', '@', '#', '$', '^', '&', '(', ')'}
+    invalid_chars = set(punctuation + whitespace) - valid_chars
+
+    for char in invalid_chars:
+        if char in new_password:
+            return False
+
+    password_has_digit = False
+
+    for char in password:
+        if char in digits:
+            password_has_digit = True
+            break
+
+    if not password_has_digit:
+        return False
+
+    password_has_lowercase = False
+
+    for char in password:
+        if char in ascii_lowercase:
+            password_has_lowercase = True
+            break
+
+    if not password_has_lowercase:
+        return False
+
+    password_has_uppercase = False
+
+    for char in password:
+        if char in ascii_uppercase:
+            password_has_uppercase = True
+            break
+
+    if not password_has_uppercase:
+        return False
+
+    return True
+
+
+def validate_input(username: str) -> bool:
+    if len(username) < 4 or len(username) > 45:
+        return False
+    return True
+
+
+# def is_authenticated(token: str) -> bool:   #!!!! Trqbwa da e swyrzano s mariaDb
+#     # note: this token is not particulary secure, use JWT for real-world uses
+#     user_id, username = token.split(_SEPARATOR)
+#     data = read_query('''
+#     SELECT id, username
+#     FROM users
+#     WHERE id = ?''',
+#                       (user_id, ))
+#     if data[0]:
+#         return True
+#     return False
